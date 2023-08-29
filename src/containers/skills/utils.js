35574014b2febf8447d10d27ed594c7efcef3634
QaziSharjeel <132188686+QaziSharjeel@users.noreply.github.com>
2023-08-29 16:02:04 +0500

    Add files via upload

---
+++ src/containers/skills/utils.js
@@ -0,0 +1,42 @@
+export const skillsData = [
+    {
+      label: "FRONT END",
+      data: [
+        {
+          skillName: "HTML",
+          percentage: "90",
+        },
+        {
+          skillName: "CSS",
+          percentage: "50",
+        },
+        {
+          skillName: "JAVASCRIPT",
+          percentage: "80",
+        },
+      ],
+    },
+    {
+      label: "BACK END",
+      data: [
+        {
+          skillName: "JAVA",
+          percentage: "50",
+        },
+        {
+          skillName: "PYTHON",
+          percentage: "20",
+        },
+      ]
+    },
+    {
+      label: "DATABASES",
+      data: [
+        {
+          skillName: "MONGO DB",
+          percentage: "30",
+        },
+        
+      ],
+    },
+  ];
